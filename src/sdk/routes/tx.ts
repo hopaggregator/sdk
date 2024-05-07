0da830672204b1e9f63347725a722e6c2d0641f8
Ian Macalinao <github@igm.pub>
2024-05-07 11:36:38 +0800

    attempt to fix object IDs in the generated tx block

--- src/sdk/routes/tx.ts
+++ src/sdk/routes/tx.ts
@@ -21,17 +21,22 @@ interface GetTxResponse {
 }
 
 interface CoinId {
-  object_id: string,
-  version: string,
+  object_id: string;
+  version: string;
 }
 
 interface InputToken {
-  object_id: CoinId,
-  coin_type: string,
-  amount: string
+  object_id: CoinId;
+  coin_type: string;
+  amount: string;
 }
 
-async function fetchCoins(client: HopApi, sui_address: string, coin_type: string, max=-1): Promise<InputToken[]> {
+async function fetchCoins(
+  client: HopApi,
+  sui_address: string,
+  coin_type: string,
+  max = -1,
+): Promise<InputToken[]> {
   let coins: CoinStruct[] = [];
   let cursor = null;
 
@@ -39,49 +44,65 @@ async function fetchCoins(client: HopApi, sui_address: string, coin_type: string
     let coin_response = await client.client.getCoins({
       owner: sui_address,
       coinType: coin_type,
-      cursor: cursor
+      cursor: cursor,
     });
     coins.push(...coin_response.data);
 
     // if you only want x coins
-    if(max != -1 && coins.length >= max) {
+    if (max != -1 && coins.length >= max) {
       break;
     }
 
-    if(coin_response.hasNextPage) {
+    if (coin_response.hasNextPage) {
       cursor = coin_response.nextCursor;
     } else {
       cursor = null;
     }
-  } while(cursor != null);
+  } while (cursor != null);
 
-  return coins.map(coin_struct => ({
+  return coins.map((coin_struct) => ({
     object_id: {
       object_id: coin_struct.coinObjectId,
-      version: coin_struct.version
+      version: coin_struct.version,
     },
     coin_type: coin_struct.coinType,
-    amount: coin_struct.balance
+    amount: coin_struct.balance,
   }));
 }
 
-async function fetchTx(client: HopApi, params: GetTxParams): Promise<GetTxResponse | null> {
+async function fetchTx(
+  client: HopApi,
+  params: GetTxParams,
+): Promise<GetTxResponse | null> {
   // get input coins
-  let user_input_coins: InputToken[] = await fetchCoins(client, params.sui_address, params.token_in);
+  let user_input_coins: InputToken[] = await fetchCoins(
+    client,
+    params.sui_address,
+    params.token_in,
+  );
   // add any input coins that match user type
-  let single_output_coin: InputToken[] = await fetchCoins(client, params.sui_address, params.token_out, 1);
+  let single_output_coin: InputToken[] = await fetchCoins(
+    client,
+    params.sui_address,
+    params.token_out,
+    1,
+  );
   user_input_coins.push(...single_output_coin);
 
   // gas coins
   let gas_coins: CoinId[];
-  if(params.token_in != "0x2::sui::SUI") {
-    let fetched_gas_coins = await fetchCoins(client, params.sui_address, "0x2:sui::SUI");
+  if (params.token_in != "0x2::sui::SUI") {
+    let fetched_gas_coins = await fetchCoins(
+      client,
+      params.sui_address,
+      "0x2:sui::SUI",
+    );
     gas_coins = fetched_gas_coins.map((struct) => struct.object_id);
   } else {
     gas_coins = user_input_coins.map((struct) => struct.object_id);
   }
 
-  const response = await makeRequest('tx', {
+  const response = await makeRequest("tx", {
     hop_server_url: client.options.hop_server_url,
     api_key: client.options.api_key,
     data: {
@@ -92,26 +113,55 @@ async function fetchTx(client: HopApi, params: GetTxParams): Promise<GetTxRespon
         sender_address: params.sui_address,
         user_input_coins,
         gas_coins,
-        gas_budget: params.gas_budget | 1e9
-      }
+        gas_budget: params.gas_budget | 1e9,
+      },
     },
-    method: 'post'
+    method: "post",
   });
 
-  if(response != null) {
-    const tx_block = TransactionBlock.from(response.tx);
-    console.log(response.tx);
+  if (response != null) {
+    const tx_block = createFrontendTxBlock(response.tx);
+    console.log(tx_block.serialize());
     return {
       token_in: response.trade.amount_in.token,
       token_out: response.trade.amount_out.token,
       amount_in: BigInt(response.trade.amount_in.amount),
       amount_out: BigInt(response.trade.amount_out.amount),
-      amount_out_with_fee: getAmountOutWithCommission(response.trade.amount_out.amount, client.options.fee_bps),
-      transaction: tx_block
+      amount_out_with_fee: getAmountOutWithCommission(
+        response.trade.amount_out.amount,
+        client.options.fee_bps,
+      ),
+      transaction: tx_block,
     };
   }
 
   return null;
 }
+
+const createFrontendTxBlock = (serialized: string): TransactionBlock => {
+  const txb = TransactionBlock.from(serialized);
+  const newInputs = txb.blockData.inputs.map((input) => {
+    if (input.type === "object") {
+      const objectId =
+        input.value?.Object?.Shared?.objectId ??
+        input.value?.Object?.ImmOrOwned?.objectId;
+      if (!objectId) {
+        throw new Error(`Missing object ID for input ${input.index}`);
+      }
+      return {
+        ...input,
+        value: objectId,
+      };
+    }
+    return input;
+  });
+  return TransactionBlock.from(
+    JSON.stringify({
+      ...txb.blockData,
+      gasConfig: {},
+      inputs: newInputs,
+    }),
+  );
+};
 
 export { GetTxParams, GetTxResponse, fetchTx };
--- src/tests/tx.test.ts
+++ src/tests/tx.test.ts
@@ -1,3 +1,4 @@
+import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { HopApi } from "../sdk/api";
 import { getFullnodeUrl } from "@mysten/sui.js/client";
 
@@ -15,7 +16,7 @@ async function quoteTest() {
       "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN",
     sui_address:
       "0x4466fe25550f648a4acd6823a90e1f96c77e1d37257ee3ed2d6e02a694984f73",
-    gas_budget: 1e9,
+    gas_budget: 1e6,
   });
 
   console.log("result", result);
