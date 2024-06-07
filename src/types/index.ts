26a9445b8aedf1f1a6a008d56a4f8319991c0339
Bonkman <bonkman223@gmail.com>
2024-06-07 10:48:02 -0400

    export trade types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,6 @@ export type { GetQuoteParams, GetQuoteResponse } from "../sdk/routes/quote.js";
 export type { VerifiedToken, GetTokensResponse } from "../sdk/routes/tokens.js";
 export type { GetTxParams, GetTxResponse } from "../sdk/routes/tx.js";
 
+export * from "../sdk/types/trade.js";
+
 export type { HopApiOptions } from "../sdk/api.js";
